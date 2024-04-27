04bfbecc89f4e92fd3c8177ea1fed71c18fe24b6
alex1001A <alexiordanov1998@gmail.com>
2024-04-27 11:16:16 +0300

    add cart

---
+++ client/src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+//React components
+import React from 'react'
+
+//Styles
+import './Cart.scss'
+
+function Cart() {
+  return (
+    <div className='cart'>Cart</div>
+  )
+}
+
+export default Cart
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -4,9 +4,15 @@ import React, { useState } from 'react'
 //Styles
 import './Product.scss'
 
+//MUI icons
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import BalanceIcon from '@mui/icons-material/Balance';
+
 export const Product = () => {
 
   const [selectedImg, setSelectedImg] = useState(0)
+  const [quantity, setQuantity] = useState(1)
 
   const images = [
     'http://images.pexels.com/photos/10026491/pexels-photo-10026491.jpeg?auto=compress&cs=tinysrgb&w=1600',
@@ -25,7 +31,38 @@ export const Product = () => {
         </div>
       </div>
       <div className="right">
-
+        <h1>Title</h1>
+        <span className='price'>$199</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, corrupti esse voluptate aut sit voluptas, fugit tenetur alias laudantium rem voluptatum perferendis nisi distinctio cumque omnis facere cupiditate totam? Illum.</p>
+        <div className="quantity">
+          <button onClick={() => setQuantity(prev => prev === 1 ? 1 : prev - 1)}>-</button>
+          {quantity}
+          <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
+        </div>
+        <button className='add'>
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISHLIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
       </div>
     </div>
   )
